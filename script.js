86b7b154c0cacf43998f8fe65aff8abc1568cb74
esdel <esdeloj@gmail.com>
2020-06-15 23:04:37 +0900

    이벤트 및 리뷰 작성 위치 변경
    이벤트 및 리뷰 작성 위치 변경

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+var reviewStart = document.getElementById('reviewButton')
+var reviewComplete = document.getElementById('reveiwComplete')
+var copyClipboard = document.getElementById('copyClipboard')
+
+// <!-- 리뷰작성 바로가기 버튼 클릭 -->
+reviewStart.addEventListener("click",moveToReview)
+function moveToReview(){
+  location.href="#reviewTitle"
+}
 // <!-- 클립보드 복사 완료 alert -->
+copyClipboard.addEventListener("click",clipboard)
 function clipboard(){
     alert('링크주소가 복사되었습니다.');
 }
 // <!-- 리뷰 작성 완료 alert -->
-function submitcomplete(){
+reviewComplete.addEventListener("click",completeMessage)
+function completeMessage(){
     alert('리뷰 작성이 완료되었습니다. 이벤트 응모 완료!');
 }
